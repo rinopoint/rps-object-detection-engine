84f166f1a40d97964c5dc51c59840d9eda47c1c9
Jon Rose <jonr@jons-mbp.lan>
2023-10-10 15:37:39 -0600

    .

--- bin/print-fake-engine.sh
+++ bin/print-fake-engine.sh
@@ -6,26 +6,46 @@ CLIENT="rps"
 GROUP_NAME="my-group-of-images-10-10-2023"
 IMAGE_FILE_URLS=$INPUT_CLIENTS_DIR/$CLIENT/$GROUP_NAME
 
-echo "Test Data: $IMAGE_FILE_URLS"
-echo "URL Format: input/clients/<CLIENT>/<GROUP NAME>/<Image Files>" 
+echo ""
+echo "===================="
+echo "General Requirements"
+echo "===================="
+echo "- Configuration by client"
+echo "- Configuration should include the ability to specify 1-n: coco dataset classes"
+echo "- Configuration should include the ability to specify 1-n: custom dataset classes"
+echo "- What else should we make configurable?" 
+echo ""
+
+
+echo "================================="
+echo "Requirement #1 - Object Detection"
+echo "================================="
+echo "Use Case: Process images in directories organized by client and group names. Per image, ouptut a json file with all the descovered data about the image." 
+echo ""
+echo "-> Input URL Format: input/clients/<CLIENT>/<GROUP NAME>/<Image Files>" 
 
 count=0;
 for FILE in `ls "$IMAGE_FILE_URLS/"`; do
-  filePrefix="${FILE%%.*}"
-
-  echo ""
-  echo "Process Image File: $FILE"
-  echo "Process & Create JSON File: $OUTPUT_CLIENTS_DIR/$GROUP_NAME/$GROUP_NAME--$filePrefix.json"
-  echo "JSON Obj Mock/Example: { client: "rps", groupName: "my-group-of-images-10-10-2023", classes: [ { className: "motorcycles", probability: 0.59 }, { className: "skateboard", probability: 0.99 }] }"
-
-  if [[ $count == 9 ]] 
+  if [[ $count == 1 ]] 
   then 
-    echo ""
-    echo "Stopping after 10 images... Fake Demo"
     break;
   fi
 
+  filePrefix="${FILE%%.*}"
+
+  echo "-> Example Image File: $FILE"
+  echo "-> Ouptut JSON Filename: $OUTPUT_CLIENTS_DIR/$GROUP_NAME/$GROUP_NAME--$filePrefix.json"
+  echo "-> Output JSON - Obj Mock/Example: { client: "rps", groupName: "my-group-of-images-10-10-2023", classes: [ { className: "motorcycles", probability: 0.59 }, { className: "skateboard", probability: 0.99 }] }"
+
   ((count=count+1))
 done
+
+
+echo ""
+echo "===================================="
+echo "Requirement #2 - Train Object, Human"
+echo "===================================="
+echo "Use Case: A script that trains a new custom class based on a directory of images." 
+echo ""
 
 
